2d341630b73d685735e38f2d05443e5ff7e02aac
Ihor Voitiuk <i.voitiuk@bluebisystems.nl>
2024-03-03 20:49:08 +0100

    Added and registered components

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from '.pages/coaches/CoachesList.vue';
+import CoachesDetail from '.pages/coaches/CoachesDetail.vue';
+import CoachRegistration from '.pages/coaches/CoachRegistration.vue';
+import ContactCoach from '.pages/requests/ContactCoach.vue';
+import RequestReceived from '.pages/requests/RequestReceived.vue';
+import NotFound from '.pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', redirect: '/coaches' },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/coaches',
+      name: 'coaches',
+      component: CoachesList
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/coaches/:id',
+      name: 'coaches',
+      component: CoachesDetail ,
+      children: [
+        {path: 'contact', component: ContactCoach }
+      ]
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: CoachRegistration
+    },
+    {
+      path: '/request',
+      name: 'request',
+      component: RequestReceived
+    },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
-})
+});
 
-export default router
+export default router;
