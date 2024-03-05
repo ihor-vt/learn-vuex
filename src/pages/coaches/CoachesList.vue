c22565e847c2e08f917fb4ed4979d2818d03d4c3
Ihor Voitiuk <i.voitiuk@bluebisystems.nl>
2024-03-05 23:22:25 +0100

    Added base components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,19 +3,32 @@
         Filter
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">{{ coach.firstName }}</li>
-        </ul>
-        <h3>No coaches found.</h3>
+        <base-card>
+            <div class="controls">
+                <base-button mode="outline">Refresh</base-button>
+                <base-button link to="/register">Register as Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <!-- <li v-for="coach in filteredCoaches" :key="coach.id">{{ coach.firstName }}</li> -->
+                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                    :last-namr="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+            </ul>
+            <h3>No coaches found.</h3>
+        </base-card>
     </section>
 </template>
 
 <script>
+import BaseCard from '@/components/ui/BaseCard.vue';
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import BaseButton from '@/components/ui/BaseButton.vue';
+
 export default {
+    components: {
+        CoachItem,
+        BaseCard,
+        BaseButton
+    },
     computed: {
         filteredCoaches() {
             return this.$store.grtters['coaches/coaches']
@@ -26,3 +39,16 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.controls {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
